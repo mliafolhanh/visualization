927e5c5da1f1889dac266204d90206f8eb56b69d
mliafol <hanh.pth@teko.vn>
2019-05-30 09:33:48 +0700

    Visualization in Python

---
+++ dash_example/dash_example1.py
@@ -0,0 +1,38 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__,  external_stylesheets=external_stylesheets)
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+app.layout = html.Div(style = {'backgroundColor': colors['background']},
+    children = [
+    html.H1(children = 'Hello Dash',
+        style = {'textAlign': 'center',
+                 'color': colors['text']
+        }),
+    html.Div(children = 'Dash: A web application frame work for Python',
+             style = {'textAlign': 'center',
+                      'color': colors['text'] }),
+    dcc.Graph(
+        id = 'example-graph',
+        figure = {
+            'data':[
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type':'bar', 'name':'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 6], 'type':'bar', 'name':u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor':colors['background'],
+                'paper_bgcolor':colors['background'],
+                'font':{
+                    'color':colors['text']
+                },
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+if __name__ == '__main__':
+    app.run_server(debug = True)
